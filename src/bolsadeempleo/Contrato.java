9d6b6fc28d04e4fe96a1a4ac9ea3b9e1e1809a08
Jorge <jorgesumle@gmail.com>
2016-05-06 05:34:30 +0200

    Añadida licencia.

---
+++ src/bolsadeempleo/Contrato.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2016 Jorge Maldonado Ventura
+ *
+ * Este programa es software libre: usted puede redistruirlo y/o modificarlo
+ * bajo los términos de la Licencia Pública General GNU, tal y como está publicada por
+ * la Free Software Foundation; ya sea la versión 3 de la Licencia, o
+ * (a su elección) cualquier versión posterior.
+ *
+ * Este programa se distribuye con la intención de ser útil,
+ * pero SIN NINGUNA GARANTÍA; incluso sin la garantía implícita de
+ * USABILIDAD O UTILIDAD PARA UN FIN PARTICULAR. Vea la
+ * Licencia Pública General GNU para más detalles.
+ *
+ * Usted debería haber recibido una copia de la Licencia Pública General GNU
+ * junto a este programa.  Si no es así, vea <http://www.gnu.org/licenses/>.
+ */
+package bolsadeempleo;
+
+/**
+ *
+ * @author Jorge Maldonado Ventura
+ */
+public class Contrato {
+    
+}
